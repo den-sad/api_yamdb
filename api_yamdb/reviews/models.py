105babd74949f4458c03035a50e80d7e3d1c512e
Данил Кочетов <dmi194@yandex.ru>
2023-02-27 23:00:14 +0300

    Добавил сырую модель Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -7,3 +7,38 @@ class User(AbstractUser):
         'Биография',
         blank=True,
     )
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=255
+    )
+    year = models.IntegerField(
+        verbose_name='Год выхода',
+        max_length=4
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        default=None
+    )
+    description = models.TextField(
+        verbose_name='Краткое описание',
+        blank=True
+    )
+    genre = models.ForeignKey(
+        'Genre',
+        verbose_name='Жанр',
+        on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        'Category',
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
