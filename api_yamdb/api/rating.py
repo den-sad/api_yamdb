74094d07f20bc99820c6a576bb35571547fc2e8e
Dnevvs <dnevvs@yandex.ru>
2023-03-06 12:20:02 +0300

    Доработаны модели и views Review, Comment, расчет rating

--- api_yamdb/api/rating.py
+++ api_yamdb/api/rating.py
@@ -8,7 +8,6 @@ def update_rating(title):
         id=title.id).annotate(avg_rating=Avg('reviews__score'))
     if t[0].avg_rating:
         rating_value = int(round(t[0].avg_rating, 0))
-    print(rating_value)
     title.rating = rating_value
     title.save()
     return title
